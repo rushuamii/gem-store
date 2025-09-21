d8520509248e2d15283ae48bf294a1ab667b671a
Umair <riyasimpair@gmail.com>
2025-09-21 16:02:56 +0530

    Added light purple border to ImageGallery component

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -16,9 +16,13 @@ export default function ImageGallery({ images }: iAppProps) {
   };
   return (
     <div className="grid gap-4 lg:grid-cols-5">
+      {/* Small images */}
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
         {images.map((image: any, idx: any) => (
-          <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
+          <div
+            key={idx}
+            className="overflow-hidden rounded-lg bg-gray-100 border border-purple-300"
+          >
             <Image
               src={urlFor(image).url()}
               width={200}
@@ -31,7 +35,8 @@ export default function ImageGallery({ images }: iAppProps) {
         ))}
       </div>
 
-      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+      {/* Big image */}
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4 border-2 border-purple-300">
         <Image
           src={urlFor(bigImage).url()}
           alt="Photo"
